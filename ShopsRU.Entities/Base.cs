1e8ec40b98b6617115f0202b13645e6eabb99ea9
gerald-credrails <gerald@credrails.com>
2021-11-01 23:05:42 +0100

    Add project files.

---
+++ ShopsRU.Entities/Base.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ShopsRU.Entities
+{
+    public class Base
+    {
+        public int Id { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+    }
+}
